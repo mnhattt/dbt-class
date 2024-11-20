8b17790b958b9a22505662793e3cb6cbacb7f602
Nhat Truong <minhnhat10bk@gmail.com>
2024-11-20 16:35:13 +0700

    step 4: add source

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,12 +1,3 @@
-with customers as (
-  select
-    *
-  from
-    { { ref('stg_jaffle_shop_customers') } }
-)
-select
-  customers.customer_id,
-  customers.first_name,
-  customers.last_name
-from
-  customers
+with customers as (select * from {{ ref("stg_jaffle_shop_customers") }})
+select customers.customer_id, customers.first_name, customers.last_name
+from customers
--- models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -1,8 +1,2 @@
-select
-  id as order_id,
-  user_id as customer_id,
-  order_date,
-  status,
-  _etl_loaded_at
-from
-  `dbt-tutorial.jaffle_shop.orders`
+select id as order_id, user_id as customer_id, order_date, status, _etl_loaded_at
+from {{ source("jaffle_shop", "orders") }}
